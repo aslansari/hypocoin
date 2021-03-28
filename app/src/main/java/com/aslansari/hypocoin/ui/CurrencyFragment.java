ba67d729fe413f137dc2caec42e0c7749b714ce4
Aslan Sarı <aslansari.dev@gmail.com>
2021-03-28 21:23:38 +0300

    - added navigation bar and moved currency list to a fragment

---
+++ app/src/main/java/com/aslansari/hypocoin/ui/CurrencyFragment.java
@@ -0,0 +1,111 @@
+package com.aslansari.hypocoin.ui;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.aslansari.hypocoin.HypoCoinApp;
+import com.aslansari.hypocoin.R;
+import com.aslansari.hypocoin.repository.model.Currency;
+import com.aslansari.hypocoin.ui.adapters.CurrencyAdapter;
+import com.aslansari.hypocoin.viewmodel.CoinViewModel;
+
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.subscribers.DisposableSubscriber;
+import timber.log.Timber;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link CurrencyFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class CurrencyFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+    private CoinViewModel coinViewModel;
+    private CurrencyAdapter currencyAdapter;
+    private CompositeDisposable disposables;
+
+    public CurrencyFragment() {
+        // Required empty public constructor
+    }
+
+    public static CurrencyFragment newInstance() {
+        CurrencyFragment fragment = new CurrencyFragment();
+        Bundle args = new Bundle();
+        // add params if necessary
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            // get params if exists
+        }
+        coinViewModel = ((HypoCoinApp) getActivity().getApplication()).appContainer.coinViewModel;
+        disposables = new CompositeDisposable();
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_currency, container, false);
+        recyclerView = view.findViewById(R.id.recyclerCoin);
+        currencyAdapter = new CurrencyAdapter();
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity(),
+                RecyclerView.VERTICAL, false);
+        recyclerView.setLayoutManager(layoutManager);
+        DividerItemDecoration dividerItemDecoration =
+                new DividerItemDecoration(recyclerView.getContext(), layoutManager.getOrientation());
+        recyclerView.addItemDecoration(dividerItemDecoration);
+        recyclerView.setAdapter(currencyAdapter);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        disposables.add(coinViewModel.getCurrencyList()
+                .subscribeWith(new DisposableSubscriber<Currency>() {
+                    @Override
+                    protected void onStart() {
+                        super.onStart();
+                    }
+
+                    @Override
+                    public void onNext(Currency currency) {
+                        currencyAdapter.add(currency);
+                    }
+
+                    @Override
+                    public void onError(@io.reactivex.annotations.NonNull Throwable e) {
+                        Timber.e(e);
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Timber.d("onComplete");
+                    }
+                }));
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        disposables.dispose();
+    }
+}
--- app/src/main/java/com/aslansari/hypocoin/ui/MainActivity.java
+++ app/src/main/java/com/aslansari/hypocoin/ui/MainActivity.java
@@ -1,33 +1,19 @@
 package com.aslansari.hypocoin.ui;
 
 import android.os.Bundle;
-import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.DividerItemDecoration;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-import com.aslansari.hypocoin.HypoCoinApp;
 import com.aslansari.hypocoin.R;
-import com.aslansari.hypocoin.repository.model.Currency;
-import com.aslansari.hypocoin.ui.adapters.CurrencyAdapter;
-import com.aslansari.hypocoin.viewmodel.CoinViewModel;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import io.reactivex.MaybeObserver;
-import io.reactivex.annotations.NonNull;
 import io.reactivex.disposables.CompositeDisposable;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.observers.DisposableMaybeObserver;
-import io.reactivex.observers.DisposableObserver;
-import io.reactivex.subscribers.DisposableSubscriber;
-import timber.log.Timber;
 
 public class MainActivity extends AppCompatActivity {
 
-    private RecyclerView recyclerView;
-    private CoinViewModel coinViewModel;
-    private CurrencyAdapter currencyAdapter;
+    private BottomNavigationView bottomNavigationView;
     private CompositeDisposable disposables;
 
     @Override
@@ -35,40 +21,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        disposables = new CompositeDisposable();
-        recyclerView = findViewById(R.id.recyclerCoin);
-        coinViewModel = ((HypoCoinApp) getApplication()).appContainer.coinViewModel;
-        currencyAdapter = new CurrencyAdapter();
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this,
-                RecyclerView.VERTICAL, false);
-        recyclerView.setLayoutManager(layoutManager);
-        DividerItemDecoration dividerItemDecoration =
-                new DividerItemDecoration(recyclerView.getContext(), layoutManager.getOrientation());
-        recyclerView.addItemDecoration(dividerItemDecoration);
-        recyclerView.setAdapter(currencyAdapter);
+        bottomNavigationView = findViewById(R.id.bottomNavBarMain);
 
-        disposables.add(coinViewModel.getCurrencyList()
-                .subscribeWith(new DisposableSubscriber<Currency>() {
-                    @Override
-                    protected void onStart() {
-                        super.onStart();
-                    }
-
-                    @Override
-                    public void onNext(Currency currency) {
-                        currencyAdapter.add(currency);
-                    }
-
-                    @Override
-                    public void onError(@NonNull Throwable e) {
-                        Timber.e(e);
-                    }
-
-                    @Override
-                    public void onComplete() {
-                        Timber.d("onComplete");
-                    }
-                }));
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            switch (item.getItemId()){
+                case R.id.nav_home:
+                    // TODO implement
+                    break;
+                case R.id.nav_currency:
+                    fragmentTransaction.replace(R.id.frameLayout, CurrencyFragment.newInstance()).commit();
+                    break;
+            }
+            return true;
+        });
+        bottomNavigationView.setSelectedItemId(R.id.nav_home);
     }
 
     @Override
